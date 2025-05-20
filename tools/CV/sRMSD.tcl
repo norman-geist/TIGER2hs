9004adfad2bd89260f93a661150506d4dd7fa8fa
Norman Geist <norman.geist@uni-greifswald.de>
2025-05-20 09:01:34 +0200

    Fix sRMSD.tcl scripted colvar. Change diversity measure to be based on range of values.

--- tools/CV/sRMSD.tcl
+++ tools/CV/sRMSD.tcl
@@ -89,65 +89,139 @@ proc rotate_coords {coords R} {
     return $result
 }
 
-# Kabsch rotation
-proc kabsch_rotation {P Q} {
-    set H [list {0.0 0.0 0.0} {0.0 0.0 0.0} {0.0 0.0 0.0}]
-    for {set i 0} {$i < [llength $P]} {incr i} {
-        foreach {px py pz} [lindex $P $i] break
-        foreach {qx qy qz} [lindex $Q $i] break
-        lset H 0 0 [expr {[lindex $H 0 0] + $qx * $px}]
-        lset H 0 1 [expr {[lindex $H 0 1] + $qx * $py}]
-        lset H 0 2 [expr {[lindex $H 0 2] + $qx * $pz}]
-        lset H 1 0 [expr {[lindex $H 1 0] + $qy * $px}]
-        lset H 1 1 [expr {[lindex $H 1 1] + $qy * $py}]
-        lset H 1 2 [expr {[lindex $H 1 2] + $qy * $pz}]
-        lset H 2 0 [expr {[lindex $H 2 0] + $qz * $px}]
-        lset H 2 1 [expr {[lindex $H 2 1] + $qz * $py}]
-        lset H 2 2 [expr {[lindex $H 2 2] + $qz * $pz}]
-    }
-
-    # Approximate orthonormalization (normalize rows)
-    set U {}
-    for {set i 0} {$i < 3} {incr i} {
-        set row [lindex $H $i]
-        set norm 0.0
-        foreach x $row { set norm [expr {$norm + $x*$x}] }
-        set norm [expr {sqrt($norm)}]
-        set norm_row {}
-        foreach x $row { lappend norm_row [expr {$x / $norm}] }
-        lappend U $norm_row
-    }
-
-    return [transpose_matrix $U]
+# Compute dot product of two 3D vectors
+proc dot {a b} {
+    expr {[lindex $a 0]*[lindex $b 0] + [lindex $a 1]*[lindex $b 1] + [lindex $a 2]*[lindex $b 2]}
 }
+
+# Compute cross product of two 3D vectors
+proc cross {a b} {
+    list \
+        [expr {[lindex $a 1]*[lindex $b 2] - [lindex $a 2]*[lindex $b 1]}] \
+        [expr {[lindex $a 2]*[lindex $b 0] - [lindex $a 0]*[lindex $b 2]}] \
+        [expr {[lindex $a 0]*[lindex $b 1] - [lindex $a 1]*[lindex $b 0]}]
+}
+
+# Normalize a 3D vector
+proc normalize {v} {
+    set norm [expr {sqrt([dot $v $v])}]
+    if {$norm == 0} {
+        return {0 0 0}
+    }
+    list \
+        [expr {[lindex $v 0] / $norm}] \
+        [expr {[lindex $v 1] / $norm}] \
+        [expr {[lindex $v 2] / $norm}]
+}
+
+# Build 3x3 zero matrix
+proc zero_matrix {} {
+    return {{0.0 0.0 0.0} {0.0 0.0 0.0} {0.0 0.0 0.0}}
+}
+
+# Add outer product of vectors a^T * b to matrix M
+proc accumulate_covariance {M a b} {
+    for {set i 0} {$i < 3} {incr i} {
+        for {set j 0} {$j < 3} {incr j} {
+            lset M $i $j [expr {[lindex $M $i $j] + [lindex $a $i] * [lindex $b $j]}]
+        }
+    }
+    return $M
+}
+
+# Transpose a 3x3 matrix
+proc transpose3x3 {M} {
+    list \
+        [list [lindex $M 0 0] [lindex $M 1 0] [lindex $M 2 0]] \
+        [list [lindex $M 0 1] [lindex $M 1 1] [lindex $M 2 1]] \
+        [list [lindex $M 0 2] [lindex $M 1 2] [lindex $M 2 2]]
+}
+
+# Determinant of 3x3 matrix via scalar triple product (more stable)
+proc determinant3x3 {M} {
+    set u [list [lindex $M 0 0] [lindex $M 1 0] [lindex $M 2 0]]
+    set v [list [lindex $M 0 1] [lindex $M 1 1] [lindex $M 2 1]]
+    set w [list [lindex $M 0 2] [lindex $M 1 2] [lindex $M 2 2]]
+    return [dot $u [cross $v $w]]
+}
+
+# Approximate orthonormalization (polar decomposition using Gram-Schmidt)
+proc orthonormalize {M} {
+    # Columns of M
+    set u0 [list [lindex $M 0 0] [lindex $M 1 0] [lindex $M 2 0]]
+    set u1 [list [lindex $M 0 1] [lindex $M 1 1] [lindex $M 2 1]]
+    set u2 [list [lindex $M 0 2] [lindex $M 1 2] [lindex $M 2 2]]
+
+    set e0 [normalize $u0]
+
+    set proj01 [expr {[dot $u1 $e0]}]
+    set temp1 [list \
+        [expr {[lindex $u1 0] - $proj01 * [lindex $e0 0]}] \
+        [expr {[lindex $u1 1] - $proj01 * [lindex $e0 1]}] \
+        [expr {[lindex $u1 2] - $proj01 * [lindex $e0 2]}]]
+    set e1 [normalize $temp1]
+
+    set e2 [normalize [cross $e0 $e1]]
+
+    # Construct matrix with orthonormalized columns
+    return [transpose3x3 [list $e0 $e1 $e2]]
+}
+
+# Main Kabsch rotation function (no reflections)
+proc kabsch_rotation {P Q} {
+    if {[llength $P] != [llength $Q]} {
+        error "Point sets must be same length"
+    }
+
+    set H [zero_matrix]
+    for {set i 0} {$i < [llength $P]} {incr i} {
+        set p [lindex $P $i]
+        set q [lindex $Q $i]
+        set H [accumulate_covariance $H $q $p]  ;# H = sum(q_i * p_i^T)
+    }
+    
+    set R [orthonormalize $H]
+
+    # Reject mirror solution if det(R) < 0
+    #puts "determinant: [determinant3x3 $R]"
+    if {[determinant3x3 $R] < 0} {
+        # Flip last column
+        for {set i 0} {$i < 3} {incr i} {
+            lset R $i 2 [expr {-[lindex $R $i 2]}]
+        }
+    }
+
+    return $R
+}
+
 
 # RMSD between aligned fragments
-proc rmsd {A B} {
+proc rmsd {P Q} {
+    set n [llength $P]
     set sum 0.0
-    set N [llength $A]
-    for {set i 0} {$i < $N} {incr i} {
-        foreach {x1 y1 z1} [lindex $A $i] break
-        foreach {x2 y2 z2} [lindex $B $i] break
-        set dx [expr {$x1 - $x2}]
-        set dy [expr {$y1 - $y2}]
-        set dz [expr {$z1 - $z2}]
+    for {set i 0} {$i < $n} {incr i} {
+        set a [lindex $P $i]
+        set b [lindex $Q $i]
+        set dx [expr {[lindex $a 0] - [lindex $b 0]}]
+        set dy [expr {[lindex $a 1] - [lindex $b 1]}]
+        set dz [expr {[lindex $a 2] - [lindex $b 2]}]
         set sum [expr {$sum + $dx*$dx + $dy*$dy + $dz*$dz}]
     }
-    return [expr {sqrt($sum / $N)}]
+    return [expr {sqrt($sum / $n)}]
 }
 
-# Main function: calc_sRMSD
 proc calc_sRMSD {args} {
 
     global sRMSD_window
-    
     set window $sRMSD_window
+
     set coordsA [lindex $args 0]
     set coordsB [lindex $args 1]
 
     if {[llength $coordsA] != [llength $coordsB]} {
         error "Coordinate list lengths must match"
     }
+
     set natoms [expr {[llength $coordsA] / 3}]
     set vecsA {}
     set vecsB {}
@@ -164,7 +238,7 @@ proc calc_sRMSD {args} {
 
         set comA [center_of_mass $fragA]
         set comB [center_of_mass $fragB]
-
+        
         set fragA0 [translate_coords $fragA $comA]
         set fragB0 [translate_coords $fragB $comB]
 
